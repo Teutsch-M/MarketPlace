88e6ef36e76dd667f8ffb6befa5a25ae72bbd94c
teutsch.l.mihaly <mlteutsch@gam>
2021-11-24 11:44:27 +0200

    style

--- Bazaar/app/src/main/java/com/example/bazaar/MainActivity.kt
+++ Bazaar/app/src/main/java/com/example/bazaar/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.bazaar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onStart()")
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
