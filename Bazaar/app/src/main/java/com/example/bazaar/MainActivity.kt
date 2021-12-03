b0793c74f935b78998dc9f7593af36113f41cff1
Teutsch-M <mlteutsch@gmail.com>
2021-12-03 21:48:57 +0200

    style

--- Bazaar/app/src/main/java/com/example/bazaar/MainActivity.kt
+++ Bazaar/app/src/main/java/com/example/bazaar/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG,"OnCreate()")
         setContentView(R.layout.activity_main)
 
     }
